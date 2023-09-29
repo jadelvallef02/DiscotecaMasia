a92bf403893474244aaccf382f921409b4337710
JoseAngel <limonconsal999@gmail.com>
2023-09-29 19:56:37 +0200

    Conectada con servicios de Google
    He conectado la aplicación con firebase, firestore y he creado la pagina de inicio de sesion, normal o con google.

--- app/src/main/java/com/example/discotecamasia/MainActivity.kt
+++ app/src/main/java/com/example/discotecamasia/MainActivity.kt
@@ -1,11 +1,149 @@
 package com.example.discotecamasia
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import com.example.discotecamasia.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    val RC_SIGN_IN = 1
+    lateinit var auth: FirebaseAuth
+    var db: FirebaseFirestore = FirebaseFirestore.getInstance()
+    var usersRef: CollectionReference = db.collection("usuarios")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val analy: FirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message","Integración completada")
+        analy.logEvent("InitScreen",bundle)
+
+
+        title = "Autenticación"
+        binding.btRegistrar.setOnClickListener {
+            if (binding.edEmail.text.isNotEmpty() && binding.edPass.text.isNotEmpty()){
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(binding.edEmail.text.toString(),binding.edPass.text.toString()).addOnCompleteListener {
+                    if (it.isSuccessful){
+                        val usuario = hashMapOf(
+                            "activado" to false,
+                            "email" to binding.edEmail.text,
+                            "rol" to "usuario"
+                        )
+                        usersRef
+                            .add(usuario)
+                            .addOnSuccessListener { documentReference ->
+                                Log.d("Jose", "Documento agregado con ID: ${documentReference.id}")
+                            }
+                            .addOnFailureListener { e ->
+                                Log.w("Jose", "Error al agregar documento", e)
+                            }
+                        irHome(it.result?.user?.email?:"",ProviderType.BASIC)
+                    } else {
+                        showAlert()
+                    }
+                }
+            }
+        }
+
+        binding.btLogin.setOnClickListener {
+            if (binding.edEmail.text.isNotEmpty() && binding.edPass.text.isNotEmpty()){
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(binding.edEmail.text.toString(),binding.edPass.text.toString()).addOnCompleteListener {
+                    if (it.isSuccessful){
+                        irHome(it.result?.user?.email?:"",ProviderType.BASIC)
+                    } else {
+                        showAlert()
+                    }
+                }
+            }
+        }
+
+        binding.btGoogle.setOnClickListener {
+            val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.request_id_token))
+                .requestEmail()
+                .build()
+
+            val googleClient = GoogleSignIn.getClient(this,googleConf)
+            googleClient.signOut()
+            val signInIntent = googleClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+
+
+        }
+
+
+        session()
+    }
+
+
+    private fun session(){
+        Log.e("Jose","Pasamos a la segunda ventana con los datos de la sesión.")
+        val prefs: SharedPreferences = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
+        val email:String? = prefs.getString("email",null)
+        val provider:String? = prefs.getString("provider", null)
+        if (email != null){
+            irHome(email, ProviderType.valueOf(provider.toString()))
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d("Jose", "firebaseAuthWithGoogle:" + account.id)
+                if (account != null) {
+                    val credential: AuthCredential = GoogleAuthProvider.getCredential(account.idToken, null)
+                    FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener {
+                        if (it.isSuccessful){
+                            irHome(account.email?:"",ProviderType.GOOGLE)  //Esto de los interrogantes es por si está vacío el email.
+                        } else {
+                            showAlert()
+                        }
+                    }
+                }
+            } catch (e: ApiException) {
+
+                showAlert()
+            }
+        }
+    }
+
+
+    private fun showAlert(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autenticando al usuario")
+        builder.setPositiveButton("Aceptar",null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun irHome(email:String, provider:ProviderType){
+        val homeIntent = Intent(this, PaginaIntermedia::class.java).apply {
+            putExtra("email",email)
+            putExtra("provider",provider.name)
+        }
+        startActivity(homeIntent)
     }
 }
---
+++ app/src/main/java/com/example/discotecamasia/ProviderType.kt
@@ -0,0 +1,6 @@
+package com.example.discotecamasia
+
+enum class ProviderType {
+    BASIC,
+    GOOGLE
+}
